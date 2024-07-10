3debccd46c6c1d2ddeed6d5cf2a001322912044e
Jaskaran Chawla <jptjasc@gmail.com>
2024-07-10 12:26:58 -0600

    Created Trainable Code

--- trainGame.py
+++ trainGame.py
@@ -1,11 +1,11 @@
-from gym import Env
+from gymnasium import Env
 from ForzaGame import ForzaGame 
-from gym.spaces import Discrete, Box, Dict, Tuple
+from typing import Optional
+from gymnasium.spaces import Discrete, Box, Dict, Tuple
 import numpy as np
 import random
 import time
 import pyautogui
-import gym
 
 class GameEnv(Env):
     def __init__(self):
@@ -15,10 +15,10 @@ class GameEnv(Env):
 
         self.action_space = Discrete(6)
         # Twemperature array  x, y, detecting attempts to cheat the system 
-        self.observation_space = Tuple((Box(-1,1, shape=(1,)), Box(-1,1, shape=(1,)), Discrete(8)), seed= 42)
+        self.observation_space = Box(low= np.array([-1, 1, 0]), high = np.array([-1,1,8]), shape=(3,), dtype= np.int32)
         print("Shape " + str(self.observation_space))
         # Set start temp
-        self.state = np.array([0,0,0], dtype=np.float32)
+        self.observation = np.array([0,0,0], dtype=np.float32)
 
         self.reward = 0
 
@@ -32,7 +32,8 @@ class GameEnv(Env):
         for key in keys:
             pyautogui.keyUp(key) 
     def step(self, action):
-        done = False
+        truncated = False
+        terminated = False
         if action == 0:
             pyautogui.keyDown("w")
         elif action == 1:
@@ -53,18 +54,18 @@ class GameEnv(Env):
 
         if self.GameController.checkGameUpdate():                   #If the game finishes as true
             self.reward = self.game_length / self.GametimeAmount * 100
-            done = True
+            terminated = True
         
         self.reward += self.GameController.points
-        self.state[0] = self.GameController.distanceX
-        self.state[1] = self.GameController.distanceY
-        self.state[2] = self.GameController.falseAlerts
+        self.observation[0] = self.GameController.distanceX
+        self.observation[1] = self.GameController.distanceY
+        self.observation[2] = self.GameController.falseAlerts
         # Reduce shower length by 1 second
         self.game_length -= 1 
         
         if self.game_length <= 0: 
             self.reward - 10000
-            done = True
+            terminated = True
 
         print(self.game_length)
         # Apply temperature noise
@@ -73,7 +74,7 @@ class GameEnv(Env):
         info = {}
         
         # Return step information
-        return self.state, self.reward, done, info
+        return self.observation, self.reward, terminated, truncated, info
 
     def render(self):
         # Implement viz
@@ -85,8 +86,13 @@ class GameEnv(Env):
             pyautogui.press('right')
             pyautogui.press('down')
 
-
-    def reset(self):
+    #def reset(self, seed)
+    def reset(self,
+        *,
+        seed: Optional[int] = None,
+        return_info: bool = False,
+        options: Optional[dict] = None,
+    ):
         # Reset shower temperature
         print("Running reset script" + str(self.game_length))
         self.reward = 0
@@ -118,16 +124,16 @@ class GameEnv(Env):
             pyautogui.press('enter')
         time.sleep(3)               # Allow game to load after rest
         
-        self.state = np.array([0,0,0], dtype=np.float32)
+        self.observation = np.array([0,0,0], dtype=np.float32)
 
 
         self.GameController.falseAlerts = 0 
         # Reset shower time
         self.game_length = self.GametimeAmount 
         self.GameController.consistenCheckFails = 0
-        return self.state
+        return (self.observation , {})
     
-#pyautogui.getWindowsWithTitle("Forza Horizon 5")[0].activate()
+pyautogui.getWindowsWithTitle("Forza Horizon 5")[0].activate()
 env = GameEnv()
 #env = gym.make("CartPole-v1")
 
@@ -153,7 +159,7 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam  
 
-states = [len(env.observation_space)]
+#states = [len(env.observation_space)]
 actions = env.action_space.n
 
 print("Actions: " + str(actions))
@@ -165,30 +171,22 @@ def build_model(states, actions):
     model.add(Dense(actions, activation='linear'))
     return model
 
-model = build_model(states, actions)
+#model = build_model(states, actions)
 
 
-model.summary()
-from rl.agents import DQNAgent
-from rl.policy import BoltzmannQPolicy
-from rl.memory import SequentialMemory
-
-def build_agent(model, actions):
-    policy = BoltzmannQPolicy()
-    memory = SequentialMemory(limit=50000, window_length=1)
-    dqn = DQNAgent(model=model, memory=memory, policy=policy, 
-                  nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
-    return dqn
+#model.summary()
 
 
-dqn = build_agent(model, actions)
-
-dqn.compile(Adam(learning_rate = 0.001), metrics= ["mae"])
-
-dqn.fit(env, nb_steps=10000, visualize=False, verbose=1)
+from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.env_checker import check_env
 
 
-scores = dqn.test(env, nb_episodes=50, visualize=False)
-print(np.mean(scores.history['episode_reward']))
+env = DummyVecEnv([lambda: GameEnv()])
+env.reset()
+#check_env(env)
 
-dqn.save_weights('forza_model.h5f', overwrite=True)
+model = DQN('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
+
+model.save("dqn_forza_model")
